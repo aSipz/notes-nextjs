7fe78f658a246b965ca53921b3cd223f5da4b559
aSipz <115708388+aSipz@users.noreply.github.com>
2023-08-22 15:29:28 +0300

    added api and server side props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,24 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 
-export default () => {
+export default ({ content }) => {
     return (
         <div sx={{ height: `calc(100vh - 60px)` }}>
             <div sx={{ variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%' }}>
-                <h1 sx={{ fontSize: 8, my: 0 }}>This is a note taking app.</h1>
+                <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
             </div>
         </div>
     );
-} 
+}
+
+export function getStaticProps(context) {
+    // console.log(context);
+
+    return {
+        props: {
+            content: {
+                title: 'This is a note taking app with Next.js'
+            }
+        }
+    }
+}
